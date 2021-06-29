48623462c8fc4259912a0c1091eab0f7c9b666d8
Bin Zheng <binzheng@bins-air.wifi.local.cmu.edu>
2021-06-29 13:40:16 -0400

    added average time spent per step

--- exploratory_analysis/get_preds.R
+++ exploratory_analysis/get_preds.R
@@ -68,9 +68,14 @@ logdata_t %>%
   mutate(time_stamp = as.POSIXlt(`CF (tool_event_time)`, 
                                  format = "%Y-%m-%d %H:%M:%OS", 
                                  tz = "UTC")) %>%
-  summarise(tot_time = max(time_stamp) - min(time_stamp),
-            adj_tot_time = duration(time_stamp)) %>%
-  rename(username = `Anon Student Id`) -> df4
+  summarise(t = max(time_stamp) - min(time_stamp),
+            a = duration(time_stamp)) %>%
+  rename(username = `Anon Student Id`) -> dddd
+
+df4 <- dddd %>%
+  group_by(username) %>%
+  summarise(tot_time = sum(t),
+            adj_tot_time = sum(a)) -> df4
 
 
 # Gets the condition, pretest score (CK + PK), and posttest scores (CK and PK seperately)
@@ -80,9 +85,12 @@ prepost %>%
 
 # Getting everything together
 df_a <- inner_join(df1, df2, by = "username")
-df_b <- inner_join(df3, df5, by = "username")
-df <- inner_join(df_a, df_b, by = "username") %>%
-  mutate(perc_hint_steps = steps_with_hints / tot_steps) %>%
+df_b <- inner_join(df3, df4, by = "username")
+df_c <- inner_join(df_a, df_b, by = "username") 
+df <- inner_join(df_c, df5, by = "username") %>%
+  mutate(perc_hint_steps = steps_with_hints / tot_steps,
+         avg_time_per_step = tot_time / tot_steps,
+         adj_avg_tps = adj_tot_time / tot_steps) %>%
   select(-c(tot_steps, steps_with_hints))
 
 write_csv(df, DATA_OUTPATH)
